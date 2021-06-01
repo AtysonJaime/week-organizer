e5b5e0f7418215eac0a677ae853bd827cd2d0fa8
Atyson Jaime <atysonjaime@gmail.com>
2021-06-01 18:00:00 -0300

    :art: Cria componente para apresenta os erros do form

---
+++ src/components/AlertErro.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notification is-danger is-light" v-show='erros.length != 0'>
+    <p><IconForm classes='fas fa-exclamation' /> Erros encontrados:</p>
+    <ul>
+      <li v-for="erro in erros" :key="erro">{{erro}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import IconForm from './Icones_forms.vue';
+
+export default {
+  components: {
+    IconForm,
+  },
+
+  props: {
+    erros: Object,
+  },
+};
+</script>
