1caabf483e3ec2fe224f04b727f56bd78502af04
Atyson Jaime <atysonjaime@gmail.com>
2021-06-02 16:21:27 -0300

    :zap: Adiciona funcionalidade para apagar todas as atividades

--- src/components/Atividades.vue
+++ src/components/Atividades.vue
@@ -1,24 +1,30 @@
 <template>
   <section class="section container">
-      <div class="columns block">
-        <div class="column is-8">
-          <h2 class="subtitle is-4">
-            Nessa aba, você poderá salvar suas atividades realizadas
-            em projetos durante a semana do seu expediente.
-            Desse modo, tornando mais fácil a citação delas nas reuniões de equipe.
-          </h2>
-        </div>
-        <div class="column is-4 button-add">
-          <button
-            class="button is-link is-medium is-fullwidth"
-            data-target="modal-atividade-add" aria-haspopup="true"
-            @click="openModal('modal-atividade-add')" title="Adicionar Atividade"
-          >
-            Adicionar Atividade
-          </button>
-          <ModalAddAtiv @criacaoAtividade = 'addAtividade($event)'/>
-        </div>
+    <div class="columns block">
+      <div class="column is-8">
+        <h2 class="subtitle is-4">
+          Nessa aba, você poderá salvar suas atividades realizadas
+          em projetos durante a semana do seu expediente.
+          Desse modo, tornando mais fácil a citação delas nas reuniões de equipe.
+        </h2>
       </div>
+      <div class="column is-4 button-add">
+        <button
+          class="button is-link is-medium is-fullwidth"
+          data-target="modal-atividade-add" aria-haspopup="true"
+          @click="openModal('modal-atividade-add')" title="Adicionar Atividade"
+        >
+          Adicionar Atividade
+        </button>
+        <ModalAddAtiv @criacaoAtividade = 'addAtividade($event)'/>
+      </div>
+    </div>
+    <article class="message is-warning msg-alert" v-show="cardAtividades.length === 0">
+      <div class="message-body">
+        <p><Icone classes='fas fa-exclamation'/> <b>Nenhuma atividade adicionada</b></p>
+      </div>
+    </article>
+    <div v-show="cardAtividades.length > 0">
       <div class="container div-cards columns is-multiline">
         <CardAtividade
           v-for="card in cardAtividades"
@@ -27,15 +33,29 @@
           :dataDado='card.data'
           :primDado='card.projeto'
           :secunDado='card.atividades'
+          :idDado='card.id'
+          @deletarAtividade='dltAtividade($event)'
         />
       </div>
-    </section>
+      <div class="content-button">
+        <button
+          class="button is-danger is-outlined"
+          title="Apagar toda lista de atividades"
+          @click='dltTodasAtividades()'
+        >
+          <Icone classes='fas fa-trash-alt'/>
+          <span>Apagar toda lista</span>
+        </button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 import ModalAddAtiv from './modais/ModalAddAtividades.vue';
 import FunctionModais from './mixins/functionModais';
 import CardAtividade from './cards/CardTipo1.vue';
+import Icone from './Icones.vue';
 
 export default {
   name: 'Atividades',
@@ -52,6 +72,7 @@ export default {
   components: {
     ModalAddAtiv,
     CardAtividade,
+    Icone,
   },
 
   methods: {
@@ -66,6 +87,10 @@ export default {
 
     dltAtividade($event) {
       this.cardAtividades = this.cardAtividades.filter((item) => item.id !== $event.idAtv);
+    },
+
+    dltTodasAtividades() {
+      this.cardAtividades = [];
     },
   },
 };
