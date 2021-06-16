5485cfd30d8e60971c6410a7e10cea056315d7fc
Atyson Jaime <atysonjaime@gmail.com>
2021-06-16 19:05:31 -0300

    :sparkles: Adiciona header do projeto

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,37 +1,37 @@
 <template>
   <nav class="navbar">
     <div class="container">
-      <div class="navbar-brand">
-        <a class="navbar-item">
-          <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
-        </a>
-        <span class="navbar-burger" data-target="navbarMenuHeroB">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      </div>
-      <div id="navbarMenuHeroB" class="navbar-menu">
-        <div class="navbar-end">
-          <a class="navbar-item is-active">
-            Home
-          </a>
-          <a class="navbar-item">
-            Examples
-          </a>
-          <a class="navbar-item">
-            Documentation
-          </a>
-          <span class="navbar-item">
-            <a class="button is-info is-inverted">
-              <span class="icon">
-                <i class="fab fa-github"></i>
-              </span>
-              <span>Download</span>
-            </a>
+      <span class="navbar-item">
+        <a class="button is-info is-inverted" @click.prevent='changeAltoContraste'>
+          <span class="icon">
+            <i class="fas fa-adjust"></i>
           </span>
-        </div>
-      </div>
+          <span>Alto Contraste</span>
+        </a>
+      </span>
     </div>
   </nav>
 </template>
+
+<script>
+export default {
+  methods: {
+    changeAltoContraste() {
+      const pagina = document.getElementById('app');
+      if (pagina.classList.contains('alto-contraste')) {
+        pagina.classList.remove('alto-contraste');
+      } else {
+        pagina.classList.add('alto-contraste');
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .navbar-item {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+</style>
